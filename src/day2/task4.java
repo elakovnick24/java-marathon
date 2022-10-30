f785293c30b7a9413384bc58ae929b1e268c3524
Nick <nelakov24@gmail.com>
2022-10-30 19:02:59 +0300

    Complete day2 tasks

---
+++ src/day2/task4.java
@@ -0,0 +1,22 @@
+package day2;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class task4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+        double y;
+        if (x >= 5) {
+            y = (Math.pow(x, 2) - 10) / (x + 7);
+        } else if (x > 3 && x < 5) {
+            y = (x + 3) * (Math.pow(x, 2) - 2);
+        } else {
+            y = 420;
+        }
+        System.out.println(y);
+    }
+}
