d7d2344e528f5681c0506fb46932a187dabfb697
Nick24 <nelakov24@gmail.com>
2022-11-18 21:57:41 +0300

    day4.task2 - fix initialize variable

--- src/day4/task2.java
+++ src/day4/task2.java
@@ -10,15 +10,15 @@ public class task2 {
      * @param args
      */
     public static void main(String[] args) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
         int [] array = new int[100];
         Random random = new Random();
 
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(10000);
         }
-
-        int min = 10000;
-        int max = 0;
 
         for (int x : array) {
             if(x < min) {
@@ -33,21 +33,14 @@ public class task2 {
         System.out.println(max);
 
         int counter = 0;
-        for (int value : array) {
-            if(value % 10 == 0) {
-                counter++;
-            }
-        }
-
-        System.out.println(counter);
-
         int sumOfValue = 0;
         for (int value : array) {
             if(value % 10 == 0) {
+                counter++;
                 sumOfValue += value ;
             }
         }
-
+        System.out.println(counter);
         System.out.println(sumOfValue);
     }
     
---
+++ src/day5/task1.java
@@ -0,0 +1,7 @@
+package day5;
+
+public class task1 {
+    public static void main(String[] args) {
+        
+    }
+}
