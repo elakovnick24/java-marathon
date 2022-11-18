6aaea58df52e3929793e1bcc44670692d9452bdd
Nick24 <nelakov24@gmail.com>
2022-11-18 16:19:27 +0300

    refactor da3.task1: - delete System.out.println in switch-case

--- src/day3/task1.java
+++ src/day3/task1.java
@@ -9,45 +9,24 @@ public class task1 {
 
         switch(country) {
             case "Moscow" : 
-            System.out.println("Russia");
-            break;
-
             case "Rostov" : 
-            System.out.println("Russia");
-            break;
-
             case "Vladivostok" : 
             System.out.println("Russia");
             break;
 
             case "Rome" : 
-            System.out.println("Italy");
-            break;
-
             case "Milan" : 
             System.out.println("Italy");
             break;
 
             case "Liverpool" : 
-            System.out.println("England");
-            break;
-
             case "Manchester" : 
-            System.out.println("England");
-            break;
-
             case "London" : 
             System.out.println("England");
             break;
 
             case "Berlin" : 
-            System.out.println("Germany");
-            break;
-
             case "Munhen" : 
-            System.out.println("Germany");
-            break;
-
             case "Keln" : 
             System.out.println("Germany");
             break;
